1c472218be13250127dc9f47446b969c5add3eb0
AFTOSETTO <AFTOSETTO@WS-LAB08_128.univel.univel.br>
2017-06-12 21:16:46 -0300

    Criação do projeto.

---
+++ meusistema/src/main/java/univel/br/prova/Produto.java
@@ -0,0 +1,29 @@
+package univel.br.prova;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String quantidade;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(String quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+
+
+}
